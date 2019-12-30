d7d72328371becf42d23af594ec1ab47e26f1547
Tiffany Curran <tcurran101@gmail.com>
2019-12-30 19:23:10 +0000

    safety commit

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,5 +1,6 @@
 class Gram < ApplicationRecord
-  validates :message, :picture, presence: true
+  validates :message, presence: true
+  validates :picture, presence: true
 
   belongs_to :user
 
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,5 @@
+# config/initializers/carrierwave.rb
+
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_public = false
